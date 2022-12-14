3101aeef3c8e3d2b81cc7fd962ed462011c31077
bentom01 <bt010003@infolab01>
2022-12-14 17:16:51 +0100

    new

--- es9_1/reco.py
+++ es9_1/reco.py
@@ -18,12 +18,25 @@ class Hit:
     def __init__(self, mod, sens, time):
         self.modulo = mod
         self.sensore = sens
-        self.timestamp = time
-
+        self.time = time
+    def __gt__(self, altro):
+        return self.time > altro.time
+    def __lt__(self, altro):
+        return self.time < altro.time
+    def __eq__(self, altro):
+        return self.time == altro.time
+ 
 class Event:
-    def __init__(self, n, tf, tl, dt, arr):
-        self.numero = n
-        self.timefirst = tf
-        self.timelast = tl
-        self.deltat = dt
-        self.array = arr
+    def __init__(self):
+        self.numero = 0
+        self.timefirst = -1
+        self.timelast = -1
+        self.deltat = -1
+        self.hit = np.empty(0)
+    def aggiungi(self, hit):
+        self.hit = np.append(self.hit, hit)
+        self.numero = len(self.hit)
+        if len(self.hit) == 1:
+            self.timefirst = hit.time
+        self.timelast = hit.time
+        self.durata = self.timelast - self.timefirst
